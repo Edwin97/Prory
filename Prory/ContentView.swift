c30a3cb25ec6b6abf205d027163162fe033e9715
edwin <c.edwin2011@gmail.com>
2020-07-19 14:23:23 +0800

    Initial Commit

---
+++ Prory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Prory
+//
+//  Created by edwin on 19/07/2020.
+//  Copyright © 2020 edwin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
