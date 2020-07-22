1e38d6529627ef2a4c95bd743e728296a3b73e2f
edwin <c.edwin2011@gmail.com>
2020-07-23 00:08:39 +0800

    Revert "Put navigation view around tab bar view instead"
    This reverts commit 780a53e34e3cd94cf09209c703aa906a9002404d.

--- Prory/View/Home/HomeView.swift
+++ Prory/View/Home/HomeView.swift
@@ -9,101 +9,103 @@
 import SwiftUI
 
 struct HomeView: View {
-	
-	var body: some View {
-		
-		ZStack {
-			Color("background")
-				.edgesIgnoringSafeArea(.all)
-			VStack() {
-				VStack(alignment: .leading) {
-					
-					HStack(spacing: 10){
-						Text("Dashboard")
-							.font(.title)
-							.fontWeight(.bold)
-							.foregroundColor(.white)
-						
-						Spacer()
-						
-						Button(action: {
-							
-						}) {
-							Image(systemName: "bell")
-								.font(.system(size: 23))
-								.foregroundColor(.white)
-						}
-					}
-					
-					HStack(spacing: 10){
-						Image("wallet")
-							.resizable()
-							.frame(width: 25, height: 25)
-							.padding()
-							.foregroundColor(.white)
-							.background(Color.black.opacity(0.06))
-							.cornerRadius(15)
-						VStack(alignment: .leading){
-							Text("Total Rent Revenue")
-								.font(.subheadline)
-								.foregroundColor(.white)
-							Text("RM 43,00,00")
-								.bold()
-								.font(.title)
-								.foregroundColor(.white)
-						}
-						Spacer()
-					}
-					
-					HStack {
-						NavigationLink(destination: PropertyView()) {
-							Text("View My Properties")
-								
-								.font(.subheadline)
-								.foregroundColor(.white)
-							Image(systemName: "arrow.right")
-								.foregroundColor(.white)
-						}
-						
-					}
-				}
-				.padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)!)
-				.padding()
-				.background(Image("dashboard-background")
-				.resizable()
-				.scaledToFill()
-				)
-				
-				HStack {
-					VStack(alignment: .leading, spacing: 5) {
-						Text("Summary")
-							.font(.headline)
-						Text("Key statistics of your account")
-							.font(.subheadline)
-					}
-					Spacer()
-				}
-				.padding()
-				
-				HStack(spacing: 15) {
-					SummaryView(item: KeySummary(id: UUID(), imageName: "property", total: "04", title: "Managed property"))
-					SummaryView(item: KeySummary(id: UUID(), imageName: "lease", total: "11", title: "Rental Leases"))
-				}
-				
-				HStack(spacing: 15) {
-					SummaryView(item: KeySummary(id: UUID(), imageName: "service", total: "88", title: "Service Requests"))
-					SummaryView(item: KeySummary(id: UUID(), imageName: "inspection", total: "31", title: "House Inpsections"))
-				}
-				
-				Spacer(minLength: 0)
-			}
-			.edgesIgnoringSafeArea(.all)
-		}
-	}
+    
+    var body: some View {
+        
+        NavigationView {
+            ZStack {
+            Color("background")
+                .edgesIgnoringSafeArea(.all)
+            VStack() {
+                VStack(alignment: .leading) {
+                    
+                    HStack(spacing: 10){
+                        Text("Dashboard")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "bell")
+                                .font(.system(size: 23))
+                                .foregroundColor(.white)
+                        }
+                    }
+                    
+                    HStack(spacing: 10){
+                        Image("wallet")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.black.opacity(0.06))
+                            .cornerRadius(15)
+                        VStack(alignment: .leading){
+                            Text("Total Rent Revenue")
+                                .font(.subheadline)
+                                .foregroundColor(.white)
+                            Text("RM 43,00,00")
+                                .bold()
+                                .font(.title)
+                                .foregroundColor(.white)
+                        }
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        NavigationLink(destination: PropertyView()) {
+                            Text("View My Properties")
+                                                       
+                                                       .font(.subheadline)
+                                                       .foregroundColor(.white)
+                                                   Image(systemName: "arrow.right")
+                                                       .foregroundColor(.white)
+                        }
+        
+                    }
+                }
+                .padding(.top, (UIApplication.shared.windows.first?.safeAreaInsets.top)!)
+                .padding()
+                .background(Image("dashboard-background")
+                .resizable()
+                .scaledToFill()
+                )
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("Summary")
+                            .font(.headline)
+                        Text("Key statistics of your account")
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                }
+                .padding()
+                
+                HStack(spacing: 15) {
+                    SummaryView(item: KeySummary(id: UUID(), imageName: "property", total: "04", title: "Managed property"))
+                    SummaryView(item: KeySummary(id: UUID(), imageName: "lease", total: "11", title: "Rental Leases"))
+                }
+                
+                HStack(spacing: 15) {
+                    SummaryView(item: KeySummary(id: UUID(), imageName: "service", total: "88", title: "Service Requests"))
+                    SummaryView(item: KeySummary(id: UUID(), imageName: "inspection", total: "31", title: "House Inpsections"))
+                }
+                
+                Spacer(minLength: 0)
+            }
+            .edgesIgnoringSafeArea(.top)
+        }
+        }
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
-	static var previews: some View {
-		HomeView()
-	}
+    static var previews: some View {
+        HomeView()
+    }
 }
